552c65ce2e264f088fa12540fa2bb35e113dc1b3
aryskaliyev <aryskaliyev@alumni.nu.edu.kz>
2023-09-05 17:06:17 +0600

    Developing day 25

--- snippetbox/cmd/web/handlers_test.go
+++ snippetbox/cmd/web/handlers_test.go
@@ -1,65 +1,22 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
-
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
 func TestPing(t *testing.T) {
-	app := &application{
-		errorLog: log.New(ioutil.Discard, "", 0),
-		infoLog: log.New(ioutil.Discard, "", 0),
-	}
-
-	ts := httptest.NewTLSServer(app.routes())
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
+	code, _, body := ts.get(t, "/ping")
 
-	if rs.StatusCode != http.StatusOK {
-		t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
-	}
-
-	defer rs.Body.Close()
-
-	body, err := ioutil.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
+	if code != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, code)
 	}
 
 	if string(body) != "OK" {
 		t.Errorf("want body to equal %q", "OK")
 	}
-
-//	rr := httptest.NewRecorder()
-
-//	r, err := http.NewRequest("GET", "/", nil)
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-
-//	ping(rr, r)
-
-//	rs := rr.Result()
-
-//	if rs.StatusCode != http.StatusOK {
-//		t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
-//	}
-
-//	defer rs.Body.Close()
-//	body, err := ioutil.ReadAll(rs.Body)
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-
-//	if string(body) != "OK" {
-//		t.Errorf("want body to equal %q", "OK")
-//	}
 }
--- snippetbox/cmd/web/testutils_test.go
+++ snippetbox/cmd/web/testutils_test.go
@@ -24,4 +24,17 @@ func newTestServer(t *testing.T, h http.Handler) *testServer {
 	return &testServer{ts}
 }
 
-func (ts *testServer) get(t *testing.T, urlPath string) ()
+func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, []byte) {
+	rs, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer rs.Body.Close()
+	body, err := ioutil.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return rs.StatusCode, rs.Header, body
+}
