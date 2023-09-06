07490dfe8e145a2e0998ef5e4d168edcf416a5b6
aryskaliyev <aryskaliyev@alumni.nu.edu.kz>
2023-09-06 17:17:38 +0600

    Developing day 26

--- snippetbox/pkg/models/mock/users.go
+++ snippetbox/pkg/models/mock/users.go
@@ -24,6 +24,15 @@ func (m *UserModel) Insert(name, email, password string) error {
 	}
 }
 
+func (m *UserModel) Authenticate(email, password string) (int, error) {
+	switch email {
+	case "alice@example.com":
+		return 1, nil
+	default:
+		return 0, models.ErrInvalidCredentials
+	}
+}
+
 func (m *UserModel) Get(id int) (*models.User, error) {
 	switch id {
 	case 1:
