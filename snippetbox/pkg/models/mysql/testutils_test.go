c1cab46a2d3bdb01fb536d295a63aa9eb9dea8bc
aryskaliyev <aryskaliyev@alumni.nu.edu.kz>
2023-09-07 19:32:08 +0600

    Developing day 27

--- snippetbox/pkg/models/mysql/testutils_test.go
+++ snippetbox/pkg/models/mysql/testutils_test.go
@@ -11,4 +11,23 @@ func newTestDB(t *testing.T) (*sql.DB, func()) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	_, err = db.Exec(string(script))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return db, func() {
+		script, err := ioutil.ReadFile("./testdata/teardown.sql")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, err = db.Exec(string(script))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		db.Close()
+	}
 }
