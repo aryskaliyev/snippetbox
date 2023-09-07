f1c4708c0ebe37263a29ac53ce2033304399f112
aryskaliyev <aryskaliyev@alumni.nu.edu.kz>
2023-09-07 19:27:51 +0600

    Developing day 27

---
+++ snippetbox/pkg/models/mysql/testutils_test.go
@@ -0,0 +1,14 @@
+package mysql
+
+import (
+	"database/sql"
+	"io/ioutil"
+	"testing"
+)
+
+func newTestDB(t *testing.T) (*sql.DB, func()) {
+	db, err := sql.Open("mysql", "test_web:pass@/test_snippetbox?parseTime=true&multiStatements=true")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
